84d550a10d672bd0b660d3b12befcc220d811c15
Pruthvisinh <prtvsinh@gmail.com>
2020-05-22 14:10:49 +0530

    change db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
+Std.create(name:'first',class_teacher:'teacher')
